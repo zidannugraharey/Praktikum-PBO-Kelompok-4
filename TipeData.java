30a6b696deed227701c373105c6839eb46f9c482
shin <zidanrey18@gmail.com>
2025-08-30 15:56:30 +0700

    Add file via upload
    Kode program untuk Laporan Praktikum BAB II Dasar Pemogramman Java

---
+++ TipeData.java
@@ -0,0 +1,37 @@
+package com.mycompany.tipedata;
+
+public class TipeData {
+
+    public static void main(String[] args) 
+    {
+        /* membuat variabel berupa 
+        1.Nama
+        2.Nim
+        3.IP tiap semester
+        4.Umur
+        5.Golongan Darah
+        Konversi 1 IP ke tipe data byte
+        Tambahkan komentar
+        Wajib ada 1 Konstanta
+        IP nya pakai array
+        */
+        
+        String nama = "Zidan Nugraha Rey";
+        final long NIM = 2407112382L;
+        String alamat; //deklarasi variabel
+        alamat = "Jl.Unggas"; //inisialisasi variabel
+        double[] IP = {3.83, 3.82}; //ip semester 1 dan 2
+        int count = 1; //untuk menampilkan semester 
+        //menampilkan output
+        System.out.println("Nama = "+ nama);
+        System.out.println("NIM = "+ NIM);
+        System.out.println("alamat = "+ alamat);
+        System.out.println(" ");
+        
+        for(int i = 0; i < IP.length;i++)
+        {
+            System.out.println("IP Semester " + count + " = " + IP[i]);
+            count++; //agar jumlah hitungan semesternya nambah
+        }
+    }
+}
