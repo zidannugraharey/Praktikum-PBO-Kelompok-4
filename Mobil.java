df96eadef736e82cbdfb104f7070ea6ea73b6615
nahdazahirah <nahdazahirah04062006@gmail.com>
2025-10-09 05:18:26 +0700

    Create Mobil.java
    BAB V

---
+++ Mobil.java
@@ -0,0 +1,38 @@
+package praktikum5;
+public class Mobil {
+    //Atribut
+    private String merk;
+    private String warna;
+    private int tahun;
+    private double harga;
+
+    //Method
+    public void nyalakanMesin(){
+        System.out.println("Mesin dinyalakan");
+    }
+    public void matikanMesin(){
+        System.out.println("Mesin dimatikan");
+    }
+
+    //Default Constructor
+    public Mobil(){
+        this.merk = "Unknown";
+        this.warna = "Unknown";
+        this.tahun = 1;
+    }
+    
+    // Constructor dengan parameter
+    public Mobil(String merk, String warna, int tahun){
+        this.merk = merk;
+        this.warna = warna;
+        this.tahun = tahun;
+    }
+    
+    public Mobil(String merk)
+    {
+        this.merk = merk;
+        this.warna = "putih";
+        this.tahun = 2023;
+        this.harga = 0.0;
+    }
+}
